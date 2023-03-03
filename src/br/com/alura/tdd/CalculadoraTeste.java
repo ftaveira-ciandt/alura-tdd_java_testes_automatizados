ff8c2857ffaa1496a1cf69f8cc1ccfb63b744f8f
Francisco Taveira <ftaveira@ciandt.com>
2023-03-03 16:23:37 -0300

    created test class

---
+++ src/br/com/alura/tdd/CalculadoraTeste.java
@@ -0,0 +1,18 @@
+package br.com.alura.tdd;
+
+public class CalculadoraTeste {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        int soma = calc.somar(3, 7);
+        System.out.println(soma);
+    }
+
+}
+
+
+
+
+
+
+
